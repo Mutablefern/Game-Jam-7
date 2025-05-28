c30c9e8be307cb48c99e0542de93e749c56ec175
TheoB28 <theo.k.f.bengtsson@gmail.com>
2025-05-28 10:46:09 +0200

    finished wack a mole and added grafik to mash

--- 2025 Game Jam Grupp 7/Assets/Theo/Script/MashPlayer.cs
+++ 2025 Game Jam Grupp 7/Assets/Theo/Script/MashPlayer.cs
@@ -6,21 +6,32 @@ using UnityEngine.InputSystem;
 public class MashPlayer : MonoBehaviour
 {
     [SerializeField] GameObject otherMashPlayer;
+    [SerializeField] Sprite ButtonSprite1;
+    [SerializeField] Sprite ButtonSprite2;
+    [SerializeField] Sprite ButtonSprite3;
+    [SerializeField] Sprite PlayerSpriteWin;
+    [SerializeField] Sprite PlayerSpriteLose;
+    [SerializeField] GameObject buttonGrafik;
+    [SerializeField] GameObject playerGrafik;
 
     int presses;
     bool canPress = true;
 
     MashPlayer otherMashPlayerScript;
-
+    SpriteRenderer PlayerSpriteRenderer;
+    SpriteRenderer ButtonSpriteRenderer;
 
     private void Awake()
     {
         otherMashPlayerScript = otherMashPlayer.GetComponent<MashPlayer>();
+        ButtonSpriteRenderer = buttonGrafik.GetComponent<SpriteRenderer>();
+        PlayerSpriteRenderer = playerGrafik.GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
         StartCoroutine(Timer());
+        ButtonSpriteRenderer.sprite = ButtonSprite1;
     }
 
     IEnumerator Timer()
@@ -31,11 +42,26 @@ public class MashPlayer : MonoBehaviour
         if (presses > otherMashPlayerScript.GetPresses())
         {
             Debug.Log(name + " wins");
+            PlayerSpriteRenderer.sprite = PlayerSpriteWin;
         }
         else if (presses == otherMashPlayerScript.GetPresses())
         {
             Debug.Log("draw");
         }
+        else
+        {
+            PlayerSpriteRenderer.sprite = PlayerSpriteLose;
+        }
+        ButtonSpriteRenderer.sprite = ButtonSprite3;
+        if (transform.position.x < 0)
+        {
+            buttonGrafik.transform.position += new Vector3(0.48f, -0.45f);
+        }
+        else
+        {
+            buttonGrafik.transform.position += new Vector3(-0.48f, -0.45f);
+        }
+        
     }
 
     void OnButtonOne(InputValue value)
@@ -43,7 +69,29 @@ public class MashPlayer : MonoBehaviour
         if (value.isPressed && canPress)
         {
             presses++;
+            StartCoroutine(Animate());
         }
+    }
+
+    IEnumerator Animate()
+    {
+        if (transform.position.x < 0)
+        {
+            ButtonSpriteRenderer.sprite = ButtonSprite2;
+            buttonGrafik.transform.position += new Vector3(0.55f, -0.25f);
+            yield return new WaitForSeconds(0.1f);
+            buttonGrafik.transform.position -= new Vector3(0.55f, -0.25f);
+            ButtonSpriteRenderer.sprite = ButtonSprite1;
+        }
+        else
+        {
+            ButtonSpriteRenderer.sprite = ButtonSprite2;
+            buttonGrafik.transform.position += new Vector3(-0.55f, -0.25f);
+            yield return new WaitForSeconds(0.1f);
+            buttonGrafik.transform.position -= new Vector3(-0.55f, -0.25f);
+            ButtonSpriteRenderer.sprite = ButtonSprite1;
+        }
+        
     }
 
     public int GetPresses()
